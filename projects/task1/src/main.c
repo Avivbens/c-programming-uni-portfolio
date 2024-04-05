2f097ded2fb6d94cd8aefdac4488e378ae3678ae
Aviv Ben Shahar <avivbens87@gmail.com>
2024-04-05 14:14:41 +0300

    chore: example for compilation of 2 files - use `RUNNER.o`

--- projects/task1/src/main.c
+++ projects/task1/src/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+int second(void);
+
 int main(void)
 {
     int testInteger;
     /* int a = 0; */
+    second();
+    printf("In main\n");
     printf("Enter an integer: ");
 
     scanf("%d", &testInteger);
---
+++ projects/task1/src/second.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int second(void)
+{
+    printf("In second\n");
+    return 0;
+}
