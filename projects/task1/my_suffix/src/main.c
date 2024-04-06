eaf9a26fc97aea4f6f823f13196e0626813d9190
Aviv Ben Shahar <avivbens87@gmail.com>
2024-04-07 00:09:34 +0300

    chore: setup task1 projects

---
+++ projects/task1/my_suffix/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "./second/second.h"
+
+int main(void) {
+    int testInteger;
+    /* int a = 0; */
+    second(1234);
+    printf("In main\n");
+    printf("Enter an integer: ");
+
+    scanf("%d", &testInteger);
+    printf("Number = %d", testInteger);
+    return 0;
+}
---
+++ projects/task1/my_suffix/src/second/second.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int second(int testNum) {
+    printf("In second, number received: %d\n", testNum);
+    return 0;
+}
