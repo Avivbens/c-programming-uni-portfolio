3853103e29c92235b5a913e877bc5c186c1c9eae
Aviv Ben Shahar <avivbens87@gmail.com>
2024-06-22 16:07:07 +0300

    chore(nx): fix `run` command to use condition checking OS

--- tools/scripts/run-project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-project_name=$1
-
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    ./dist/$project_name/$project_name.exe
-else
-    ./dist/$project_name/$project_name
-fi
