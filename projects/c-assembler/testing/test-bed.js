b7df26901300409cf3cc90ae01bab3e8cb111262
Aviv Ben Shahar <avivbens87@gmail.com>
2024-08-19 13:55:34 +0300

    feat: add tests for c-assembler

--- projects/c-assembler/testing/test-bed.js
+++ projects/c-assembler/testing/test-bed.js
@@ -1,16 +1,15 @@
 #! /usr/bin/env node
 
 const { exec } = require('node:child_process')
-const { mkdtemp, writeFile } = require('node:fs/promises')
+const { readFile } = require('node:fs/promises')
 const { resolve } = require('node:path')
-const { cwd, exit } = require('node:process')
+const { cwd } = require('node:process')
 const { promisify } = require('node:util')
 
 const execPrm = promisify(exec)
 
 const PROJECT_NAME = require('../project.json').name
-const DIST_PATH = `dist/${PROJECT_NAME}`
-const EXECUTABLE = `${DIST_PATH}/${PROJECT_NAME}`
+const RUN_COMMAND = (sourceFile) => `nx run ${PROJECT_NAME}:run --args="--input ${sourceFile}"`
 
 const COLORS = {
     RED: '\x1b[31m',
@@ -19,61 +18,48 @@ const COLORS = {
     RED_BACKGROUND: '\x1b[41m',
     GREEN_BACKGROUND: '\x1b[42m',
 }
-const CONSOLE_COLOR = (color) => `${color}%s\x1b[0m`
-
-async function createTempFile() {
-    const tempDirName = await mkdtemp('dist/test-')
-    const tempFileName = `${tempDirName}/input.txt`
-    await writeFile(tempFileName, '')
-
-    return { tempDirName, tempFileName }
-}
-
-function compareArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false
-    }
-
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return false
-        }
-    }
-
-    return true
-}
-
-function extractResults(rawOutput) {
-    const lines = rawOutput.split('\n')
-    if (!lines.length) {
-        exit(1)
-    }
-
-    const RESULTS_HEADER = `The results are: `
-    const resultsIndex = lines.findIndex((line) => line.includes(RESULTS_HEADER))
-    if (resultsIndex === -1) {
-        exit(1)
-    }
-
-    const allResults = lines.slice(resultsIndex + 1)
-
-    const parsedResults = allResults.filter(Boolean).map(s => s.trim()).map(Number)
-    return parsedResults
-}
-
-async function cleanup(tempDirectory) {
-    await execPrm(`rm -rf ${tempDirectory}`, { shell: true })
-}
+const CONSOLE_COLOR = (color) => `${color}%s\x1b[0m`;
 
 ; (async () => {
-    const { tempDirName, tempFileName } = await createTempFile()
-
     try {
         const testsConfiguration = require('./inputs-configuration.json')
 
         let errorRate = 0
 
-        // TODO: Implement the tests
+        for (const test of testsConfiguration) {
+            const { expect, sourceFile, target } = test
+            await execPrm(RUN_COMMAND(sourceFile))
+
+            const obExpectFile = await readFile(resolve(cwd(), expect.fileOutput), 'utf-8')
+            const postProcessExpectFile = await readFile(resolve(cwd(), expect.postProcessFileOutput), 'utf-8')
+            const entryExpectFile = await readFile(resolve(cwd(), expect.entryFileOutput), 'utf-8')
+            const externExpectFile = await readFile(resolve(cwd(), expect.externFileOutput), 'utf-8')
+
+            const obTargetFile = await readFile(resolve(cwd(), target.fileOutput), 'utf-8')
+            const postProcessTargetFile = await readFile(resolve(cwd(), target.postProcessFileOutput), 'utf-8')
+            const entryTargetFile = await readFile(resolve(cwd(), target.entryFileOutput), 'utf-8')
+            const externTargetFile = await readFile(resolve(cwd(), target.externFileOutput), 'utf-8')
+
+            if (obExpectFile !== obTargetFile) {
+                console.error(CONSOLE_COLOR(COLORS.RED), `Test failed: ob file is different`)
+                errorRate++
+            }
+
+            if (postProcessExpectFile !== postProcessTargetFile) {
+                console.error(CONSOLE_COLOR(COLORS.RED), `Test failed: post process file is different`)
+                errorRate++
+            }
+
+            if (entryExpectFile !== entryTargetFile) {
+                console.error(CONSOLE_COLOR(COLORS.RED), `Test failed: entry file is different`)
+                errorRate++
+            }
+
+            if (externExpectFile !== externTargetFile) {
+                console.error(CONSOLE_COLOR(COLORS.RED), `Test failed: extern file is different`)
+                errorRate++
+            }
+        }
 
         if (!errorRate) {
             console.log(CONSOLE_COLOR(COLORS.GREEN_BACKGROUND), 'All tests passed successfully')
@@ -83,6 +69,5 @@ async function cleanup(tempDirectory) {
         console.error(CONSOLE_COLOR(COLORS.RED_BACKGROUND), `${errorRate} tests failed`)
         throw new Error('Tests failed')
     } finally {
-        await cleanup(tempDirName)
     }
 })()
